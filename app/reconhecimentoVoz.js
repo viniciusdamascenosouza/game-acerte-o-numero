89941355b2d9ce44f7fb9dced30896e223336d39
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-14 14:19:29 -0300

    Web speech API

---
+++ app/reconhecimentoVoz.js
@@ -0,0 +1,7 @@
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = "pt-br";
+recognition.start();
