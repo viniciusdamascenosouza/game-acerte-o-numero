bbe799bb91355697f74bfaba9913f645d7e32805
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-11-14 14:57:53 -0300

    Mostrando a mensagem na tela

--- app/reconhecimentoVoz.js
+++ app/reconhecimentoVoz.js
@@ -1,3 +1,5 @@
+const chuteElemento = document.querySelector("#chute");
+
 window.SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 
@@ -8,4 +10,14 @@ recognition.start();
 
 recognition.addEventListener("result", (eventoEscutado) => {
   console.log(eventoEscutado.results[0][0].transcript);
+  const chute = eventoEscutado.results[0][0].transcript;
+  chuteElemento.innerHTML = `<div>
+                                <h2>Você disse:</h2>
+                             </div>
+                             <div class="box-div">
+                                <span class="box">${chute}</span>
+                             </div>
+                             <div>
+                                <h2>O número secreto é maior <i class="fa-solid fa-up-long"></i></h2>
+                             </div>`;
 });
